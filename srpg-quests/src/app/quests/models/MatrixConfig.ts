1c2085d87f090da894cc2a5f79666d3ef6bf268f
Flavio Marques <flavio.marques.inf@gmail.com>
2022-09-24 10:54:21 -0300

    Create MatrixConfig.ts

---
+++ srpg-quests/src/app/quests/models/MatrixConfig.ts
@@ -0,0 +1,13 @@
+export interface MatrixConfig {
+  title: string;
+  hideTitle: boolean;
+  xAxisLabel: string;
+  yAxisLabel: string;
+  xAxisValue: number | boolean;
+  yAxisValue: number | boolean;
+  isBothTrueText: string;
+  isBothFalseText: string;
+  isXtrueYfalseText: string;
+  isXfalseYtrueText: string;
+}
+
